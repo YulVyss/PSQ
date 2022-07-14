cf675a7be501b2741ebb08e06936ef0e4cf077a2
YulVyss <vyssotskaya.yulia@gmail.com>
2022-07-14 17:47:05 +0300

    a lot changes

--- js/index.js
+++ js/index.js
@@ -2,15 +2,15 @@ document.querySelector('body').addEventListener('click', (ev) => {
 
   if (ev.target.classList.contains('nav-mobile')) {
     ev.target.classList.toggle('active')
-    document.querySelector('.header__nav').classList.toggle('active')
+    document.querySelector('.header__nav_m').classList.toggle('active')
   }
   if (ev.target.classList.contains('nav-mobile__item')) {
     ev.target.parentNode.classList.toggle('active')
-    document.querySelector('.header__nav').classList.toggle('active')
+    document.querySelector('.header__nav_m').classList.toggle('active')
   }
   if (ev.target.classList.contains('header__nav-link')) {
     document.querySelector('.nav-mobile').classList.remove('active')
-    document.querySelector('.header__nav').classList.remove('active')
+    document.querySelector('.header__nav_m').classList.remove('active')
   }
 
 })
@@ -24,6 +24,7 @@ new simpleParallax(image1, {
 const image2 = document.getElementsByClassName('thumbnail2');
 new simpleParallax(image2, {
   overflow: true,
-  orientation: 'left'
+  orientation: 'left',
+  scale: 2
 });
 
